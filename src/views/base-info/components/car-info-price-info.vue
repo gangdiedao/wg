6123d48bd1d3ba12a63c74abfd437d39e0bcbbc2
chenshaoyang66 <158794727@qq.com>
2020-04-14 16:09:03 +0800

    车辆信息管理

--- src/views/base-info/components/car-info-price-info.vue
+++ src/views/base-info/components/car-info-price-info.vue
@@ -1,62 +1,23 @@
 <template>
   <div>
     <el-table :data="priceTableData" border style="width: 100%" :show-header="false">
-      <el-table-column v-for="(item,index) in tableLabel" :key="item.id" :prop="item.prop" :label="item.name" align="center">
+      <el-table-column v-for="(item,index) in tableLabel" :key="index" :prop="item.prop" :label="item.name" align="center">
         <template slot-scope="{row,$index}">
-          <el-input v-if="!(row.flag == 0 && $index == 0 && index == 0)" v-model="row.name" />
           <span v-if="index == 0 && $index == 0">类型/车型</span>
-          <el-button v-if="row.flag === 0 && row.id === 1" type="text" @click="XAdd">添加</el-button>
-          <el-button v-if="row.flag === 0 && row.id != 1" type="text" @click="XDelete">删除</el-button>
+          <el-button v-else-if="row.flag === 0 && index == 0" type="text" @click="XAdd">+添加</el-button>
+          <el-button v-else-if="row.flag === 0 && index != 0" type="text" @click="XDelete(index)">-删除</el-button>
+          <el-input v-else-if="index == 0" v-model="row.name" />
+          <el-input v-else-if="$index == 0" v-model="item.name" />
+          <el-input v-else />
         </template>
       </el-table-column>
       <el-table-column prop="date" label="xxx" align="center">
         <template slot-scope="{row,$index}">
-          <el-button v-if="$index === 0" type="text" @click="YAdd">添加</el-button>
-          <el-button v-else type="text" @click="YDelete">删除</el-button>
+          <el-button v-if="$index === 0" type="text" @click="YAdd">+添加</el-button>
+          <el-button v-else-if="$index === priceTableData.length-1" type="text" @click="clearAll">-清空</el-button>
+          <el-button v-else type="text" @click="YDelete($index)">-删除</el-button>
         </template>
       </el-table-column>
-      <!-- <el-table-column align="center">
-        <template slot-scope="{row}">
-          <el-input v-if="row.flag != 0" v-model="row.name" />
-          <el-button v-if="row.flag === 0" type="text" @click="XDelete">删除</el-button>
-        </template>
-      </el-table-column>
-      <el-table-column prop="date" label="司机" align="center">
-        <template slot-scope="{row}">
-          <el-input v-if="row.flag != 0" v-model="row.name" />
-          <el-button v-if="row.flag === 0" type="text" @click="XDelete">删除</el-button>
-        </template>
-      </el-table-column>
-      <el-table-column prop="date" label="电话" align="center">
-        <template slot-scope="{row}">
-          <el-input v-if="row.flag != 0" v-model="row.name" />
-          <el-button v-if="row.flag === 0" type="text" @click="XDelete">删除</el-button>
-        </template>
-      </el-table-column>
-      <el-table-column prop="date" label="类型" align="center">
-        <template slot-scope="{row}">
-          <el-input v-if="row.flag != 0" v-model="row.name" />
-          <el-button v-if="row.flag === 0" type="text" @click="XDelete">删除</el-button>
-        </template>
-      </el-table-column>
-      <el-table-column prop="date" label="状态" align="center">
-        <template slot-scope="{row}">
-          <el-input v-if="row.flag != 0" v-model="row.name" />
-          <el-button v-if="row.flag === 0" type="text" @click="XDelete">删除</el-button>
-        </template>
-      </el-table-column>
-      <el-table-column prop="date" label="备注" align="center">
-        <template slot-scope="{row}">
-          <el-input v-if="row.flag != 0" v-model="row.name" />
-          <el-button v-if="row.flag === 0" type="text" @click="XDelete">删除</el-button>
-        </template>
-      </el-table-column>
-      <el-table-column prop="date" label="备注" align="center">
-        <template slot-scope="{row,$index}">
-          <el-button v-if="$index === 0" type="text" @click="YAdd">添加</el-button>
-          <el-button v-else type="text" @click="YDelete">删除</el-button>
-        </template>
-      </el-table-column> -->
     </el-table>
     <template slot="footer">
       <el-button @click="visible = false">{{ $t('i18nView.information.cancel') }}</el-button>
@@ -73,60 +34,39 @@ export default {
   data() {
     return {
       visible: false,
+      nameArr: '新车型',
       priceTableData: [
         {
-          id: 1,
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          id: 2,
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
+          id: 0,
+          num: 0,
+          name: '新类型'
         },
         {
-          id: 3,
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          id: 4,
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        },
-        {
-          date: '',
-          name: '',
-          address: '',
+          id: 999,
+          num: 0,
+          name: '占位',
           flag: 0
         }
       ],
       tableLabel: [
         {
           id: 0,
-          name: '表1',
-          prop: ''
-        },
-        {
-          id: 1,
-          name: '表2',
+          name: '新车型',
           prop: ''
         }
       ]
     }
   },
   computed: {},
-  created() {
-    this.infoTypeList = this.infoTypeListData()
-    this.prorateTypeList = this.prorateTypeListData()
-  },
+  created() {},
   methods: {
     // 横坐标的添加
     XAdd() {
-      alert(11)
+      var obj = { ...this.priceTableData[0], ...{ id: this.priceTableData.length - 1, name: '新类型' }}
+      this.priceTableData.splice(this.priceTableData.length - 1, 0, obj)
+      this.priceTableData = JSON.parse(JSON.stringify(this.priceTableData))
+      console.log(this.priceTableData)
+      console.log(this.tableLabel)
     },
     // 横坐标的删除
     XDelete() {
@@ -134,11 +74,39 @@ export default {
     },
     // 纵坐标的添加
     YAdd() {
-      alert(22)
+      this.priceTableData.map((item, index) => {
+        item.num = item.num + 1
+        item['name' + item.num] = '新类型'
+      })
+      this.priceTableData = JSON.parse(JSON.stringify(this.priceTableData))
+      this.tableLabel.push({ id: this.tableLabel.length, name: '新车型', prop: '' })
+      console.log(this.priceTableData)
+      console.log(this.tableLabel)
     },
     // 纵坐标的删除
-    YDelete() {
-      alert(444)
+    YDelete(i) {
+      console.log(i)
+      this.priceTableData.splice(i, 1)
+    },
+    // 清空
+    clearAll() {
+      this.priceTableData = [
+        {
+          id: 1,
+          name: '新类型'
+        },
+        {
+          name: '占位',
+          flag: 0
+        }
+      ]
+      this.tableLabel = [
+        {
+          id: 0,
+          name: '新车型',
+          prop: ''
+        }
+      ]
     },
     dataFormSubmitHandle() {}
   }
