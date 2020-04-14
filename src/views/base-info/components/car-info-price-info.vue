e8eafd16880e4cc0722409627908b341c2f316e4
小刚 <x@x.x>
2020-04-14 18:01:42 +0800

    gogo

--- src/views/base-info/components/car-info-price-info.vue
+++ src/views/base-info/components/car-info-price-info.vue
@@ -8,7 +8,7 @@
           <el-button v-else-if="row.flag === 0 && index != 0" type="text" @click="XDelete(index)">-删除</el-button>
           <el-input v-else-if="index == 0" v-model="row.name" />
           <el-input v-else-if="$index == 0" v-model="item.name" />
-          <el-input v-else />
+          <el-input v-else v-model="data[$index-1][item.col-1]['value']" :value="item.col-1"/>
         </template>
       </el-table-column>
       <el-table-column prop="date" label="xxx" align="center">
@@ -33,8 +33,11 @@ export default {
   mixins: [mixin],
   data() {
     return {
+      data: [],
       visible: false,
       nameArr: '新车型',
+      x: 0,
+      y: 0,
       priceTableData: [
         {
           id: 0,
@@ -62,11 +65,18 @@ export default {
   methods: {
     // 横坐标的添加
     XAdd() {
+      this.y += 1
+      let _arr = []
+      for(let i = 0; i < this.x; i++) {
+        _arr.push({value: `${this.y}-${this.x}`})
+      }
+      this.data.push(_arr)
       var obj = { ...this.priceTableData[0], ...{ id: this.priceTableData.length - 1, name: '新类型' }}
       this.priceTableData.splice(this.priceTableData.length - 1, 0, obj)
       this.priceTableData = JSON.parse(JSON.stringify(this.priceTableData))
-      console.log(this.priceTableData)
-      console.log(this.tableLabel)
+      // console.log(this.priceTableData)
+      // console.log(this.tableLabel)
+      console.log(this.data)
     },
     // 横坐标的删除
     XDelete() {
@@ -74,14 +84,19 @@ export default {
     },
     // 纵坐标的添加
     YAdd() {
+      this.x += 1
+      this.data.forEach(item => {
+        item.push({value: `${this.y}-${this.x}`})
+      })
       this.priceTableData.map((item, index) => {
         item.num = item.num + 1
         item['name' + item.num] = '新类型'
       })
       this.priceTableData = JSON.parse(JSON.stringify(this.priceTableData))
-      this.tableLabel.push({ id: this.tableLabel.length, name: '新车型', prop: '' })
-      console.log(this.priceTableData)
-      console.log(this.tableLabel)
+      this.tableLabel.push({ id: this.tableLabel.length, name: '新车型', prop: '', col: this.x })
+      // console.log(this.priceTableData)
+      // console.log(this.tableLabel)
+      console.log(this.data)
     },
     // 纵坐标的删除
     YDelete(i) {
